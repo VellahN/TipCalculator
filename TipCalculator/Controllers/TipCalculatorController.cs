4f6a8696974ad027047b748458e6115008e8aa15
VellahN <92290865+VellahN@users.noreply.github.com>
2024-02-17 17:45:59 +0300

    Add project files.

---
+++ TipCalculator/Controllers/TipCalculatorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+public class TipCalculatorController : Controller
+{
+  public ActionResult Index()
+  {
+    return View(new MealCostModel());
+  }
+
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public ActionResult Index(MealCostModel model)
+  {
+    if (ModelState.IsValid)
+    {
+      model.CalculateTips();
+      return View(model);
+    }
+
+    return View(model);
+  }
+}
---
+++ TipCalculator/Models/TipCalculatorModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class MealCostModel
+{
+  [Required(ErrorMessage = "The cost of the meal is required.")]
+  [Range(1, double.MaxValue, ErrorMessage = "The cost of the meal must be a valid number greater than 0.")]
+  public decimal MealCost { get; set; }
+
+  public decimal Tip15Percent { get; set; }
+  public decimal Tip20Percent { get; set; }
+  public decimal Tip25Percent { get; set; }
+
+  public void CalculateTips()
+  {
+    Tip15Percent = MealCost * 0.15m;
+    Tip20Percent = MealCost * 0.20m;
+    Tip25Percent = MealCost * 0.25m;
+  }
+}
